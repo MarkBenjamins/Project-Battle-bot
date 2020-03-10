63bd90fb3aba58d8db6f76073bffdc2e91e1eee1
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-03-10 11:22:25 +0100

    Update Yellow_Cyclops2.0.ino

--- Yellow Cyclops/Botcode/Yellow_Cyclops2.0/Yellow_Cyclops2.0.ino
+++ Yellow Cyclops/Botcode/Yellow_Cyclops2.0/Yellow_Cyclops2.0.ino
@@ -346,32 +346,33 @@ void doolhofLinks()
  */
 void parcour()
 {
-//    int distance = sonar.ping_cm();
-//    String distanceLCDText = String(distance) + "cm";
-//    updateSecondLCDText(distanceLCDText);
-//    delay(500);
+    int distance = sonar.ping_cm();
+    String distanceLCDText = String(distance) + "cm";
+    updateSecondLCDText(distanceLCDText);
+    delay(500);
     delay(1000);
-    if (sonar.ping_cm() > 30)
+//    if ((sonar.ping_cm() > 30))
+    if ((sonar.ping_cm() <= 30) && (sonar.ping_cm() < 0))
     {
         updateSecondLCDText("meer dan 30cm");
         battleBotDrive.drive(24, 40);
     }
-    else if (sonar.ping_cm() <= 30)
-    {
-        updateSecondLCDText("Minder dan 30cm");
-        battleBotDrive.drive(15, -15);
-        delay(500);
-        battleBotDrive.drive(24, 40);
-        delay(1000);
-        battleBotDrive.drive(-20, 0);
-        delay(300);
-    }
-    else
-    {
-        updateSecondLCDText("Critical Error");
-        still();
-        delay(5000);
-    }
+//    else if (sonar.ping_cm() <= 30)
+//    {
+//        updateSecondLCDText("Minder dan 30cm");
+//        battleBotDrive.drive(15, -15);
+//        delay(500);
+//        battleBotDrive.drive(24, 40);
+//        delay(1000);
+//        battleBotDrive.drive(-20, 0);
+//        delay(300);
+//    }
+//    else
+//    {
+//        updateSecondLCDText("Critical Error");
+//        still();
+//        delay(5000);
+//    }
 }
 
 /**
